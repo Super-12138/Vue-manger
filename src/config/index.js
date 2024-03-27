1e874396ab6ba6f0430f4f5588b52f9115ddbb0f
Super-12138 <1669586802@qq.com>
2024-03-27 15:28:44 +0800

    修改线上mock地址

--- src/config/index.js
+++ src/config/index.js
@@ -6,27 +6,30 @@
  * 线上环境
  */
 // 当前的环境
-const env = import.meta.env.MODE || 'prod'
+const env = import.meta.env.MODE || "prod";
 
 const EnvConfig = {
   development: {
-    baseApi: '/api',
-    mockApi: 'https://www.fastmock.site/mock/300283cb590d2022f75682f11a1848b0/api',
+    baseApi: "/api",
+    mockApi:
+      // "https://www.fastmock.site/mock/300283cb590d2022f75682f11a1848b0/api",
+      "https://mock.apifox.com/m1/4068509-0-default/api",
   },
   test: {
-    baseApi: '//test.future.com/api',
-    mockApi: 'https://www.fastmock.site/mock/300283cb590d2022f75682f11a1848b0/api',
+    baseApi: "//test.future.com/api",
+    mockApi:
+      "https://www.fastmock.site/mock/300283cb590d2022f75682f11a1848b0/api",
   },
   pro: {
-    baseApi: '//future.com/api',
-    mockApi: 'https://www.fastmock.site/mock/300283cb590d2022f75682f11a1848b0/api',
+    baseApi: "//future.com/api",
+    mockApi:
+      "https://www.fastmock.site/mock/300283cb590d2022f75682f11a1848b0/api",
   },
-}
+};
 
 export default {
   env,
   // mock的总开关,
-  mock: false,
-  ...EnvConfig[env]
-}
-
+  mock: true,
+  ...EnvConfig[env],
+};
